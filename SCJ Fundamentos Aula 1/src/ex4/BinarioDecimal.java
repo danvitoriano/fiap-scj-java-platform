66f5a7622cc72286a3936c3b47a8c57f953d3d46
Michel Fernandes <michelpf@gmail.com>
2015-09-30 22:42:15 -0300

    Importação inicial.

---
+++ SCJ Fundamentos Aula 1/src/ex4/BinarioDecimal.java
@@ -0,0 +1,28 @@
+package ex4;
+public class BinarioDecimal {
+
+	public static void main(String[] args) {
+
+		int valor=12;
+		int quociente;
+		int resto;
+		String respost="";
+		
+		quociente=valor/2;
+		resto=valor%2;
+		System.out.println(resto);
+		respost=String.valueOf(resto)+respost;
+		
+		while (quociente>0){
+			resto=quociente%2;
+			quociente=quociente/2;
+			respost=String.valueOf(resto)+respost;
+			System.out.println(resto);
+		}
+
+		System.out.println(respost);
+		
+		 String bin = Integer.toString(valor, 2);  
+		 System.out.println(bin);
+	}
+}
