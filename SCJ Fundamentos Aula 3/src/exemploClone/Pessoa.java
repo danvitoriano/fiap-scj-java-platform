79d881bf49072b5921112534a39f46d28f522de0
michelfernandes <michelfernandes@172.16.62.147>
2015-10-13 18:23:23 -0300

    Exemplo de clonar um objeto

---
+++ SCJ Fundamentos Aula 3/src/exemploClone/Pessoa.java
@@ -0,0 +1,20 @@
+package exemploClone;
+
+public class Pessoa implements Cloneable{
+	
+	private String nome;
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	
+	 public Pessoa clone() throws CloneNotSupportedException{  
+         return (Pessoa) super.clone();  
+  }  
+	
+
+}
