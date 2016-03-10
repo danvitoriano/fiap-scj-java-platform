bccbe0414a5412f287469e66ec389b006b7cce73
Michel Fernandes <michelpf@gmail.com>
2016-03-09 22:54:30 -0300

    Inclusão classe de consulta.

---
+++ SCJ Persistência Aula 1/src/roteiro/br/com/fiap/aplicacao/ConsultaClientes.java
@@ -0,0 +1,41 @@
+package roteiro.br.com.fiap.aplicacao;
+
+import roteiro.br.com.fiap.dao.ClientesDao;
+import roteiro.br.com.fiap.dao.PedidosDao;
+import roteiro.br.com.fiap.entity.Clientes;
+import roteiro.br.com.fiap.entity.Pedidos;
+
+public class ConsultaClientes {
+
+	public static void main(String[] args) {
+	
+		try {
+			
+			System.out.println("Consultando Cliente ...");
+			
+			ClientesDao clientes = new ClientesDao();
+			PedidosDao pedidos = new PedidosDao();
+		
+			Clientes cliente = clientes.buscarCliente(7);
+			
+			System.out.println("Cliente "+cliente.getNome()+" "+cliente.getEmail());
+			
+			for (Pedidos pedido : cliente.getPedidos()) {
+				System.out.println(pedido.getDescricao()+" "+pedido.getValor()+" "+pedido.getData());
+			}
+			
+			System.out.println("Consultando Cliente Finalizado!");
+			System.out.println("Consultando Pedidos ...");
+			Pedidos pedido = pedidos.buscarPedido(7);
+			System.out.println(pedido.getDescricao()+" "+pedido.getValor()+" "+pedido.getData());
+			System.out.println("Consultando Pedidos Finalizado!");
+			
+			
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		
+
+	}
+
+}
