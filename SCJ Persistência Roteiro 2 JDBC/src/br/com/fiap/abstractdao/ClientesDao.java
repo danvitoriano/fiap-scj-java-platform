87aee3aaeb306885d780bdecf16ce722ba066adf
Danilo Vitoriano <vitoriano08@gmail.com>
2019-09-30 11:17:09 -0300

    remove unused

--- SCJ Persistência Roteiro 2 JDBC/src/br/com/fiap/abstractdao/ClientesDao.java
+++
@@ -1,8 +0,0 @@
-package br.com.fiap.abstractdao;
-
-import br.com.fiap.entity.Clientes;
-
-public interface ClientesDao {
-	Clientes inserirCliente(Clientes cliente) throws Exception;
-	Clientes buscarCliente(int id) throws Exception;
-}
--- SCJ Persistência Roteiro 5 JPA/src/br/com/fiap/entity/StatusCliente.java
+++
@@ -1,5 +0,0 @@
-package br.com.fiap.entity;
-
-public enum StatusCliente{
-	ATIVO, INATIVO
-}
